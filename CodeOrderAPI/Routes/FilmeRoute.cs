2ead986a64f588d07e1d8374785129871a188ed1
Guilherme Bley <guilhermecbley@gmail.com>
2024-04-24 23:07:24 -0300

    add GET endpoints

--- CodeOrderAPI/Routes/FilmeRoute.cs
+++ CodeOrderAPI/Routes/FilmeRoute.cs
@@ -1,5 +1,8 @@
 ﻿using CodeOrderAPI.Data;
 using CodeOrderAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 
 namespace CodeOrderAPI.Routes
 {
@@ -7,26 +10,93 @@ namespace CodeOrderAPI.Routes
     {
         public static void MapFilmeEndpoints(this WebApplication app)
         {
-            var summaries = new[]
+            app.MapGet("/Filme", (DataContext context, CancellationToken cancellationToken) =>
             {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+                return context
+                    .Filmes
+                    .Select(movie =>
+                        new
+                        {
+                            movie.Title,
+                            movie.Episode,
+                            movie.OpeningCrawl,
+                            movie.Director,
+                            movie.Producer,
+                            movie.ReleaseDate,
+                            Characters = movie.Characters.Select(character =>
+                                new 
+                                { 
+                                    character.Id,
+                                    character.Name,
+                                }),
+                            Planets = movie.Planets.Select(planet => 
+                                new
+                                {
+                                    planet.Id,
+                                    planet.Name,
+                                }),
+                            Vehicles = movie.Veichles.Select(vehicle => 
+                                new
+                                {
+                                    vehicle.Id, 
+                                    vehicle.Name,
+                                }),
+                            Starships = movie.Starships.Select(starship =>
+                                new
+                                {
+                                    starship.Id,
+                                    starship.Name,
+                                })
+                        })
+                    .AsNoTracking()
+                    .ToListAsync(cancellationToken);
+            });
 
-            app.MapGet("/Filme", (DataContext context) =>
+            app.MapGet("/Filme/{id}", (
+                [FromRoute]int id, 
+                DataContext context, 
+                CancellationToken cancellationToken) =>
             {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    (
-                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        Random.Shared.Next(-20, 55),
-                        summaries[Random.Shared.Next(summaries.Length)]
-                    ))
-                    .ToArray();
-                return forecast;
-                ;
-            })
-            .WithName("Filme")
-            .WithOpenApi();
+                return context
+                    .Filmes
+                    .Where(movie => movie.Id == id)
+                    .Select(movie =>
+                        new
+                        {
+                            movie.Title,
+                            movie.Episode,
+                            movie.OpeningCrawl,
+                            movie.Director,
+                            movie.Producer,
+                            movie.ReleaseDate,
+                            Characters = movie.Characters.Select(character =>
+                                new
+                                {
+                                    character.Id,
+                                    character.Name,
+                                }),
+                            Planets = movie.Planets.Select(planet =>
+                                new
+                                {
+                                    planet.Id,
+                                    planet.Name,
+                                }),
+                            Vehicles = movie.Veichles.Select(vehicle =>
+                                new
+                                {
+                                    vehicle.Id,
+                                    vehicle.Name,
+                                }),
+                            Starships = movie.Starships.Select(starship =>
+                                new
+                                {
+                                    starship.Id,
+                                    starship.Name,
+                                })
+                        })
+                    .AsNoTracking()
+                    .ToListAsync(cancellationToken);
+            });
 
         }
     }
