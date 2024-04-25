8e5a16a9744bce97874e4a63760bec999c0b3309
Guilherme Bley <guilhermecbley@gmail.com>
2024-04-24 23:33:34 -0300

    add POST endpoint

--- CodeOrderAPI/Routes/FilmeRoute.cs
+++ CodeOrderAPI/Routes/FilmeRoute.cs
@@ -1,122 +1,155 @@
 ﻿using CodeOrderAPI.Data;
-using CodeOrderAPI.Model;
+using CodeOrderAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Numerics;
 
-namespace CodeOrderAPI.Routes
+namespace CodeOrderAPI.Routes;
+
+public static class FilmeRoute
 {
-    public static class FilmeRoute
+    public static void MapFilmeEndpoints(this WebApplication app)
     {
-        public static void MapFilmeEndpoints(this WebApplication app)
+        app.MapGet("/Filme", (DataContext context, CancellationToken cancellationToken) =>
         {
-            app.MapGet("/Filme", (DataContext context, CancellationToken cancellationToken) =>
+            return context
+                .Filmes
+                .Select(movie =>
+                    new
+                    {
+                        movie.Title,
+                        movie.Episode,
+                        movie.OpeningCrawl,
+                        movie.Director,
+                        movie.Producer,
+                        movie.ReleaseDate,
+                        Characters = movie.Characters.Select(character =>
+                            new 
+                            { 
+                                character.Id,
+                                character.Name,
+                            }),
+                        Planets = movie.Planets.Select(planet => 
+                            new
+                            {
+                                planet.Id,
+                                planet.Name,
+                            }),
+                        Vehicles = movie.Veichles.Select(vehicle => 
+                            new
+                            {
+                                vehicle.Id, 
+                                vehicle.Name,
+                            }),
+                        Starships = movie.Starships.Select(starship =>
+                            new
+                            {
+                                starship.Id,
+                                starship.Name,
+                            })
+                    })
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        });
+
+        app.MapGet("/Filme/{id}", (
+            [FromRoute]int id, 
+            DataContext context, 
+            CancellationToken cancellationToken) =>
+        {
+            return context
+                .Filmes
+                .Where(movie => movie.Id == id)
+                .Select(movie =>
+                    new
+                    {
+                        movie.Title,
+                        movie.Episode,
+                        movie.OpeningCrawl,
+                        movie.Director,
+                        movie.Producer,
+                        movie.ReleaseDate,
+                        Characters = movie.Characters.Select(character =>
+                            new
+                            {
+                                character.Id,
+                                character.Name,
+                            }),
+                        Planets = movie.Planets.Select(planet =>
+                            new
+                            {
+                                planet.Id,
+                                planet.Name,
+                            }),
+                        Vehicles = movie.Veichles.Select(vehicle =>
+                            new
+                            {
+                                vehicle.Id,
+                                vehicle.Name,
+                            }),
+                        Starships = movie.Starships.Select(starship =>
+                            new
+                            {
+                                starship.Id,
+                                starship.Name,
+                            })
+                    })
+                .AsNoTracking()
+                .FirstOrDefaultAsync(cancellationToken);
+        });
+
+        app.MapDelete("/Filme/{id}", async (
+            [FromRoute] int id,
+            DataContext context,
+            CancellationToken cancellationToken) =>
+        {
+            var modelFound = await context
+                .Filmes
+                .FirstOrDefaultAsync(movie => movie.Id == id, cancellationToken);
+
+            if (modelFound is null)
+                return Results.NotFound();
+
+            context.Filmes.Remove(modelFound);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Results.Ok(modelFound);
+        });
+
+        app.MapPost("/Filme", async (
+            [FromBody] MovieToAddViewModel modelToAdd,
+            DataContext context,
+            CancellationToken cancellationToken) =>
+        {
+            var modelAlreadyAdded = await context.Filmes.FirstOrDefaultAsync(
+                movie => movie.Title == modelToAdd.Title,
+                cancellationToken);
+
+            if (modelAlreadyAdded is not null)
+                return Results.BadRequest("");
+
+            var transaction = 
+                await context.Database.BeginTransactionAsync(cancellationToken);
+
+            var modelAddedResult = await context.Filmes.AddAsync(new Model.Filme
             {
-                return context
-                    .Filmes
-                    .Select(movie =>
-                        new
-                        {
-                            movie.Title,
-                            movie.Episode,
-                            movie.OpeningCrawl,
-                            movie.Director,
-                            movie.Producer,
-                            movie.ReleaseDate,
-                            Characters = movie.Characters.Select(character =>
-                                new 
-                                { 
-                                    character.Id,
-                                    character.Name,
-                                }),
-                            Planets = movie.Planets.Select(planet => 
-                                new
-                                {
-                                    planet.Id,
-                                    planet.Name,
-                                }),
-                            Vehicles = movie.Veichles.Select(vehicle => 
-                                new
-                                {
-                                    vehicle.Id, 
-                                    vehicle.Name,
-                                }),
-                            Starships = movie.Starships.Select(starship =>
-                                new
-                                {
-                                    starship.Id,
-                                    starship.Name,
-                                })
-                        })
-                    .AsNoTracking()
-                    .ToListAsync(cancellationToken);
+                Director = modelToAdd.Director,
+                Episode = modelToAdd.Episode,
+                OpeningCrawl = modelToAdd.OpeningCrawl,
+                Producer = modelToAdd.Producer,
+                Title = modelToAdd.Title,
+                ReleaseDate = modelToAdd.ReleaseDate
             });
 
-            app.MapGet("/Filme/{id}", (
-                [FromRoute]int id, 
-                DataContext context, 
-                CancellationToken cancellationToken) =>
-            {
-                return context
-                    .Filmes
-                    .Where(movie => movie.Id == id)
-                    .Select(movie =>
-                        new
-                        {
-                            movie.Title,
-                            movie.Episode,
-                            movie.OpeningCrawl,
-                            movie.Director,
-                            movie.Producer,
-                            movie.ReleaseDate,
-                            Characters = movie.Characters.Select(character =>
-                                new
-                                {
-                                    character.Id,
-                                    character.Name,
-                                }),
-                            Planets = movie.Planets.Select(planet =>
-                                new
-                                {
-                                    planet.Id,
-                                    planet.Name,
-                                }),
-                            Vehicles = movie.Veichles.Select(vehicle =>
-                                new
-                                {
-                                    vehicle.Id,
-                                    vehicle.Name,
-                                }),
-                            Starships = movie.Starships.Select(starship =>
-                                new
-                                {
-                                    starship.Id,
-                                    starship.Name,
-                                })
-                        })
-                    .AsNoTracking()
-                    .FirstOrDefaultAsync(cancellationToken);
-            });
+            var modelAdded = modelAddedResult.Entity;
 
-            app.MapDelete("/Filme/{id}", async (
-                [FromRoute] int id,
-                DataContext context,
-                CancellationToken cancellationToken) =>
-            {
-                var modelFound = await context
-                    .Filmes
-                    .FirstOrDefaultAsync(movie => movie.Id == id, cancellationToken);
+            
 
-                if (modelFound is null)
-                    return Results.NotFound();
+            await transaction.CommitAsync(cancellationToken);
 
-                context.Filmes.Remove(modelFound);
+            await context.SaveChangesAsync(cancellationToken);
 
-                await context.SaveChangesAsync(cancellationToken);
-
-                return Results.Ok(modelFound);
-            });
-
-        }
+            throw new NotImplementedException();
+        });
     }
 }
---
+++ CodeOrderAPI/ViewModels/MovieToAddViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeOrderAPI.ViewModels;
+
+public class MovieToAddViewModel
+{
+    public string Title { get; set; } = string.Empty;
+    public int Episode { get; set; }
+    public string OpeningCrawl { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
+    public string Producer { get; set; } = string.Empty;
+    public DateTime ReleaseDate { get; set; }
+    public IEnumerable<int> Planets { get; set; } = Enumerable.Empty<int>();
+    public IEnumerable<int> Characters { get; set; } = Enumerable.Empty<int>();
+    public IEnumerable<int> Starships { get; set; } = Enumerable.Empty<int>();
+    public IEnumerable<int> Veichles { get; set; } = Enumerable.Empty<int>();
+}
