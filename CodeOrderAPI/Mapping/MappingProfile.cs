d23be763ebc2671c7a1e0de44a61d453ac1b9bec
Rodrigo Penaforte <86313462+RodrigoPenaforte@users.noreply.github.com>
2024-04-27 20:10:32 -0300

    feat: adicionado o mappeamento para view models

--- CodeOrderAPI/Mapping/MappingProfile.cs
+++ CodeOrderAPI/Mapping/MappingProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using CodeOrderAPI.Model;
+using CodeOrderAPI.ViewModels;
 
 public class MappingProfile : Profile
 {
@@ -7,5 +8,10 @@ public class MappingProfile : Profile
     {
         CreateMap<Personagem, PersonagemDTO>();
         CreateMap<PersonagemDTO, Personagem>();
+
+
+        // mapeamentos para ViewModels
+        CreateMap<PersonagemToAddViewModel, Personagem>();
+        CreateMap<PersonagemToUpdateViewModel, Personagem>();
     }
 }
